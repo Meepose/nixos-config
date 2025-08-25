5c26b61129c0f4665b5207363a6a46ff5c921c49
julian <juliandavidhayes@gmail.com>
2025-08-25 15:09:10 -0400

    added discord

--- configuration.nix
+++ configuration.nix
@@ -102,6 +102,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+  discord-ptb
   git
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
